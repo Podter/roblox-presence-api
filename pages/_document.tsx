ca947b0d1be302e8751744c5ea9d39eaa0526715
Podter <48936015+Podter@users.noreply.github.com>
2023-02-12 09:35:23 +0700

    removed boilerplate

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Expose your Roblox presence and activities to a RESTful API"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
